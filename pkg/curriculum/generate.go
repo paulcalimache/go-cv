44322b5ba34dbc3f7d060b33532be8bc48cd7a0d
paulcalimache <paul.calimache@gmail.com>
2024-03-08 22:41:47 +0100

    refacto

--- pkg/curriculum/generate.go
+++ pkg/curriculum/generate.go
@@ -1,30 +1,74 @@
 package curriculum
 
 import (
+	"bytes"
+	"context"
 	"log/slog"
 	"os"
 	"text/template"
+
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
 )
 
 func (cv *CV) Render(output string, tmplName string) error {
 	slog.Info("Rendering the " + tmplName + " template ...")
 	t := getTemplate(tmplName)
 
-	os.MkdirAll(output, os.ModePerm)
-	os.Chdir(output)
-	f, err := os.Create(tmplName + ".html")
-	if err != nil {
-		return err
-	}
-	//html
-	err = t.ExecuteTemplate(f, "classic.html", cv)
+	var file bytes.Buffer
+
+	err := t.ExecuteTemplate(&file, "classic.html", cv)
 	if err != nil {
 		return err
 	}
 
-	//pdf
-	// htmlBuf := bytes.NewBufferString("")
+	os.MkdirAll(output, os.ModePerm)
+	os.Chdir(output)
+
+	saveAsHTML(file)
+	saveAsPDF(file)
+
 	slog.Info("CV rendered at " + output)
+	return nil
+}
+
+func saveAsHTML(file bytes.Buffer) error {
+	return os.WriteFile("curriculum.html", file.Bytes(), 0644)
+}
+
+func saveAsPDF(file bytes.Buffer) error {
+	ctx, cancelCtx := chromedp.NewContext(context.Background())
+	defer cancelCtx()
+
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("about:blank"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			frameTree, err := page.GetFrameTree().Do(ctx)
+			if err != nil {
+				return err
+			}
+			return page.SetDocumentContent(frameTree.Frame.ID, file.String()).Do(ctx)
+		}),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			buf, _, err := page.
+				PrintToPDF().
+				WithMarginBottom(0).
+				WithMarginTop(0).
+				WithMarginRight(0).
+				WithMarginLeft(0).
+				WithPaperHeight(11.67).
+				WithPaperWidth(8.27).
+				WithPrintBackground(true).
+				WithPreferCSSPageSize(true).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+			return os.WriteFile("curriculum.pdf", buf, 0644)
+		}),
+	); err != nil {
+		return err
+	}
 	return nil
 }
 
